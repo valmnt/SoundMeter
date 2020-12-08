e33261fee2e1ba8bc42be3480d013eebdd5355a4
MONT <valentin.mont@ynov.com>
2020-12-08 09:24:17 +0100

    button start/stop

--- app/src/main/java/com/github/valmnt/soundmeter/SoundMeterActivity.kt
+++ app/src/main/java/com/github/valmnt/soundmeter/SoundMeterActivity.kt
@@ -1,15 +1,19 @@
 package com.github.valmnt.soundmeter
 
 import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Criteria
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.*
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import com.google.android.material.slider.Slider
 
 class SoundMeterActivity: AppCompatActivity() {
@@ -18,7 +22,7 @@ class SoundMeterActivity: AppCompatActivity() {
         const val POPUP_DISPLAYED = "popupAlreadyDisplayed"
         const val TAG_PERMISSION_FRAGMENT = "permissionDialogFragment"
         const val REQUEST_CODE_PERMISSION = 1
-        const val LOCATION_UPDATE_MS = 10_000L
+        const val LOCATION_UPDATE_MS = 1_000L
         const val LOCATION_UPDATE_RADIUS = 100f
     }
 
@@ -28,7 +32,9 @@ class SoundMeterActivity: AppCompatActivity() {
     private lateinit var latitudeView: TextView
     private lateinit var longitudeView: TextView
     private lateinit var speedLocationView: TextView
+    private lateinit var btnView: Button
     private lateinit var slider: Slider
+
     private var maxValue: Float = 0F
 
 
@@ -39,6 +45,9 @@ class SoundMeterActivity: AppCompatActivity() {
         speedLocationView.text = "speed : " + location.speed.toString()
 
         if (location.hasSpeed() && location.speed > maxValue) {
+            (getSystemService(VIBRATOR_SERVICE) as Vibrator).apply {
+                vibrate(500)
+            }
             Toast.makeText(this, "Vibrate", Toast.LENGTH_SHORT).show()
         }
     }
@@ -55,6 +64,11 @@ class SoundMeterActivity: AppCompatActivity() {
         slider.addOnChangeListener { slider, value, fromUser ->
             maxValue = value
         }
+
+        btnView = findViewById(R.id.btn)
+        btnView.setOnClickListener {
+            startOrStop()
+        }
     }
 
     override fun onResume() {
@@ -69,15 +83,9 @@ class SoundMeterActivity: AppCompatActivity() {
                         .commit()
                 popupAlreadyDisplayed = true
             }
-        } else {
-            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-            locationManager?.requestLocationUpdates(
-                    LOCATION_UPDATE_MS,
-                    LOCATION_UPDATE_RADIUS,
-                    Criteria().apply {
-                        accuracy = Criteria.ACCURACY_FINE
-                    }, locationListener, Looper.myLooper())
         }
+        syncUI()
+        btnView.isEnabled = ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED
     }
 
     override fun onPause() {
@@ -88,5 +96,31 @@ class SoundMeterActivity: AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putBoolean(POPUP_DISPLAYED, popupAlreadyDisplayed)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startOrStop() {
+        if (locationManager == null) {
+            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+            locationManager?.requestLocationUpdates(
+                    LOCATION_UPDATE_MS,
+                    LOCATION_UPDATE_RADIUS,
+                    Criteria().apply {
+                        accuracy = Criteria.ACCURACY_FINE
+                    }, locationListener, Looper.myLooper()
+            )
+        } else {
+            locationManager?.removeUpdates(locationListener)
+            locationManager = null
+        }
+        syncUI()
+    }
+
+    private fun syncUI() {
+        if (locationManager == null) {
+            btnView.text = getString(R.string.btn_start)
+        } else {
+            btnView.text = getString(R.string.btn_stop)
+        }
     }
 }
